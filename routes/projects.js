7c7d9c6749a534ce6063b38c1cc53009281996a8
rumble <dhwnsghdhkd@naver.com>
2021-05-27 00:25:15 +0900

    modify1

--- routes/projects.js
+++ routes/projects.js
@@ -10,7 +10,7 @@ router.get('/', function(req, res, next) {
   
   if (start && end) {
   var sql ='SELECT project_id, project_name, DATE_FORMAT(project_start_date,\'%Y-%m-%d\') project_start_date, DATE_FORMAT(project_terminate_date,\'%Y-%m-%d\') project_terminate_date, ' +
-            'project_type, customer_id FROM project WHERE project_start_date=\"'+start+'\" AND project_terminate_date=\"'+end+'\" ORDER BY project_terminate_date DESC';
+            'project_type, customer_id FROM project WHERE project_start_date >= \"'+start+'\" AND project_terminate_date <= \"'+end+'\"  ORDER BY project_terminate_date DESC';
   
   console.log(start);
   console.log(end);
