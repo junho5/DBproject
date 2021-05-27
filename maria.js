527c4459b7713227700d0189f0cd725de9ec739f
rumble <dhwnsghdhkd@naver.com>
2021-05-27 12:54:36 +0900

    connection

--- maria.js
+++ maria.js
@@ -7,5 +7,23 @@ const conn = maria.createConnection({
     database:'heroku_d57af3a00f950b4',
     multipleStatements: true
 });
-
+function handleDisconnect() {
+    conn.connect(function(err) {            
+      if(err) {                            
+        console.log('error when connecting to db:', err);
+        setTimeout(handleDisconnect, 2000); 
+      }                                   
+    });                                 
+                                           
+    conn.on('error', function(err) {
+      console.log('db error', err);
+      if(err.code === 'PROTOCOL_CONNECTION_LOST') { 
+        return handleDisconnect();                      
+      } else {                                    
+        throw err;                              
+      }
+    });
+}
+  
+handleDisconnect();
 module.exports = conn;
